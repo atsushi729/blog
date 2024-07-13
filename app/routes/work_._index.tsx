78e5ae41afb05ec4e480323f637893c07e1c5d38
Atsushi <atu729@icloud.com>
2024-07-13 11:24:37 +0900

    set up work page component

--- app/routes/work_._index.tsx
+++ app/routes/work_._index.tsx
@@ -1,5 +1,23 @@
-const work = () => {
-  return <div>work</div>;
-};
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
-export default work;
+import { getPosts } from "~/.server/posts";
+import { Post } from "~/components/post";
+
+export const loader = async () => json(await getPosts());
+
+export default function Component() {
+  const posts = useLoaderData<typeof loader>();
+
+  return (
+    <div className="p-10">
+      <ul className="space-y-8">
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Post {...post} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
