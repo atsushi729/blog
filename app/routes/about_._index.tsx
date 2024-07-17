9ea31c9c75d91699dd0a3d3f934a934ac45c8e78
Atsushi <atu729@icloud.com>
2024-07-17 23:14:34 +0900

    replace about page from MDX to tsx file

---
+++ app/routes/about_._index.tsx
@@ -0,0 +1,8 @@
+export default function Component() {
+  return (
+    <div className="p-10 prose">
+      <h1>Atsushi Hatakeyama</h1>
+      <p>Hi! I'm Atsushi who love Next.js, Remix and Laravel.</p>
+    </div>
+  );
+}
