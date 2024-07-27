cd10f5f56e9ce970564a41cc6b0b1e2fbd764470
Atsushi <atu729@icloud.com>
2024-07-27 17:24:47 +0900

    add dark mode theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,14 @@ import typography from "@tailwindcss/typography";
 
 export default {
   content: ["./app/**/*.{ts,tsx}"],
-
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(to right top, rgb(55, 65, 81), rgb(17, 24, 39), rgb(2, 9, 16))",
+      },
+    },
   },
   plugins: [typography],
+  darkMode: "class",
 } satisfies Config;
